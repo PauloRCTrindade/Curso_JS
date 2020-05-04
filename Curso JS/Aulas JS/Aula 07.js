018ddbf3b02a12e402063d8273b738229e913e66
Paulo R C Trindade <paulinhotrindade121212@gmail.com>
2020-05-04 15:48:54 -0300

    Create Aula 07.js

---
+++ Curso JS/Aulas JS/Aula 07.js
@@ -0,0 +1,7 @@
+var vel = 85
+
+console.log(` A velocidade do seu carro é ${vel} Km`)
+if (vel >=80){
+    console.log('Você foi multado ligeirinho')
+}
+ else{console.log('Voce esta dirigindo com seguramça')}
