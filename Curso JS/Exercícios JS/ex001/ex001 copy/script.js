afc54cc8ca0111616470baed1b8cd2a1b2947da7
Paulo R C Trindade <paulinhotrindade121212@gmail.com>
2020-05-04 23:51:56 -0300

    Create script.js

---
+++ Curso JS/Exercícios JS/ex001/ex001 copy/script.js
@@ -0,0 +1,9 @@
+
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+}
+
