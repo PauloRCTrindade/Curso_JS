82101c93c9f9659de1b0c58041c099ad7749ec98
Paulo R C Trindade <paulinhotrindade121212@gmail.com>
2020-05-07 11:17:28 -0300

    Create script.js

---
+++ Curso JS/Exercícios JS/ex003/script.js
@@ -0,0 +1,16 @@
+function enter(){
+    var num = document.getElementById('valor')
+    var tab = document.getElementById('seltabuada')
+    if(num.value.length == 0){
+        alert(' Por favor digite um número')
+    }else{
+        var n = Number(num.value)
+        tab.innerHTML = ''
+        for(c = 0; c <= 10; c++){
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+}
