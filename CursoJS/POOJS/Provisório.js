677bbe2b78cd5d9908bbc7592ef888d27385d54e
Paulo R C Trindade <paulinhotrindade121212@gmail.com>
2020-05-15 20:58:39 -0300

    Update Provisório.js

--- CursoJS/POOJS/Provisório.js
+++ CursoJS/POOJS/Provisório.js
@@ -1,37 +1,39 @@
-class Cliente{
-    
-    get(){
+class Cliente {
+
+    get() {
         this._nome = nome
         this._cpf = cpf
+        this_senha = senha
     }
-    
-    constructor(nome,cpf){
+
+    constructor(nome, cpf,senha) {
         this._nome = nome
         this._cpf = cpf
+        this.senha = senha
     }
 }
 // classe abstrata
-class Conta{
-    set cliente(novoDado){
-        if(novoDado instanceof Cliente){
-          
-        }  this._cliente = novoDado
+class Conta {
+    set cliente(novoDado) {
+        if (novoDado instanceof Cliente) {
+
+        } this._cliente = novoDado
     }
-    
-    get cliente(){
+
+    get cliente() {
         return this.cliente
     }
 
-    get saldo(){
+    get saldo() {
         return this._saldo
     }
 
-    get agencia(){
+    get agencia() {
         return this._agencia
     }
 
-    constructor(agencia,cliente,saldo){
-        if(this.constructor == Conta){
+    constructor(agencia, cliente, saldo) {
+        if (this.constructor == Conta) {
             throw new Error("Você não devia instanciar através de uma conta pai")
         }
         this._agencia = agencia
@@ -39,75 +41,75 @@ class Conta{
         this._saldo = saldo
     }
     // metodo abstrato
-    sacar(valor){
+    sacar(valor) {
         throw new Error('O método sacar é absrato')
     }
 
-    _sacar(valor,taxa){
+    _sacar(valor, taxa) {
         const valorSacado = valor * taxa
-        if(this._saldo >= valorSacado){
+        if (this._saldo >= valorSacado) {
             this._saldo -= valorSacado
-        return valorSacado
+            return valorSacado
         }
     }
 
-    depositar(valor){
-        if(valor <= 0){
+    depositar(valor) {
+        if (valor <= 0) {
             return console.log('Essa operação não pode ser realizada')
         }
 
         this._saldo += valor
         console.log(`Saldo atual ${this._saldo}`)
-        
+
     }
-    
+
 }
 
-class ContaCorrente extends Conta{
-    constructor(agencia,cliente,saldo){
-        super(agencia,cliente,saldo)
+class ContaCorrente extends Conta {
+    constructor(agencia, cliente, saldo) {
+        super(agencia, cliente, saldo)
     }
-    sacar(valor){
+    sacar(valor) {
         let taxa = 1.2
-        return this._sacar(valor,taxa)
+        return this._sacar(valor, taxa)
     }
-    
+
 }
 
-class ContaPoupanca extends Conta{
-    constructor(agencia,cliente,saldo){
-        super(agencia,cliente,saldo)
+class ContaPoupanca extends Conta {
+    constructor(agencia, cliente, saldo) {
+        super(agencia, cliente, saldo)
     }
 
-    sacar(valor){
+    sacar(valor) {
         let taxa = 1
-        return this._sacar(valor,taxa)
+        return this._sacar(valor, taxa)
     }
 }
 
-class ContaSalario extends Conta{
-    constructor(agencia,cliente,saldo){
-        super(agencia,cliente,saldo)
+class ContaSalario extends Conta {
+    constructor(agencia, cliente, saldo) {
+        super(agencia, cliente, saldo)
     }
-    sacar(valor){
+    sacar(valor) {
         let taxa = 1
-        return this._sacar(valor,taxa)
+        return this._sacar(valor, taxa)
     }
 
 }
 
-class ContaEmpresarial extends Conta{
-    constructor(agencia,cliente,conta){
-        super(agencia,cliente,conta)
+class ContaEmpresarial extends Conta {
+    constructor(agencia, cliente, conta) {
+        super(agencia, cliente, conta)
     }
-    sacar(valor){
+    sacar(valor) {
         let taxa = 1.5
-        return this._sacar(valor,taxa)
+        return this._sacar(valor, taxa)
     }
 }
 
-class Funcionario{
-    constructor(nome,salario,cpf){
+class Funcionario {
+    constructor(nome, salario, cpf) {
         this._nome = nome
         this._salario = salario
         this._cpf = cpf
@@ -115,37 +117,59 @@ class Funcionario{
         this._senha
     }
 
+    autenticar(senha) {
+        return senha == this._senha
+    }
 
-    
+    cadastrarSenha(senha) {
+        this._senha = senha
+    }
+
+
 }
 
 
 
-class ContaGerente extends Funcionario{
-    constructor(nome,salario,cpf){
-        super(nome,salario,cpf)
+class Gerente extends Funcionario {
+    constructor(nome, salario, cpf) {
+        super(nome, salario, cpf)
         this._bonificacao = 1.2
-        
+
     }
 }
 
-class ContaDiretor extends Funcionario{
-    constructor(nome,salario,cpf){
-        super(nome,salario,cpf)
+class Diretor extends Funcionario {
+    constructor(nome, salario, cpf) {
+        super(nome, salario, cpf)
         this._bonificacao = 1.5
     }
 }
 
+class SistemaAltenticacao {
+    static login(autenticavel, senha) {
+        return autenticavel.autenticar(senha) 
+    }
+}
 
 
 
 
 
-const cliente01 = new Cliente('Paulo',08914952665 )
-const cliente02 = new Cliente('Rafael',1234564569 )
-const cliente03  = new Cliente('Willian',52654123)
 
-contaCorrentePaulo = new ContaCorrente(1001,cliente01,0)
+const cliente01 = new Cliente('Paulo', 08914952665)
+const cliente02 = new Cliente('Rafael', 1234564569)
+
+const diretor = new Diretor('Willian', 1000, 456123123)
+diretor.cadastrarSenha(123456)
+
+const gerente = new Gerente('Rodrigo', 5000, 456789852)
+gerente.cadastrarSenha(7894)
+
+const diretorEstaLogado = SistemaAltenticacao.login(diretor, '123456')
+const gerenteEstaLogado = SistemaAltenticacao.login(gerente, '7894')
+console.log(diretorEstaLogado,gerenteEstaLogado)
+
+/*contaCorrentePaulo = new ContaCorrente(1001,cliente01,0)
 contaPoupancaPaulo = new ContaPoupanca(1001,cliente01,0)
 contaSalarioPaulo =  new ContaSalario(1001,cliente01,0)
 contaFuncionarioPaulo = new ContaFuncionario(1001,cliente01,0)
@@ -156,7 +180,7 @@ contaPoupancaRafael = new ContaPoupanca(2002,cliente02,0)
 
 contaEmpresarialWillian = new ContaEmpresarial(3003,cliente03,0)
 
-
+conta
 contaEmpresarialWillian.depositar(50000)
 contaEmpresarialWillian.sacar(100)
 console.log(contaEmpresarialWillian)
